b785c00fba9826b5d887aa316eaf15c083ca03ed
Hoang Vo <g@hvvo.dev>
2020-09-24 13:23:30 -0400

    Use Redis Cluster

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,7 +1,11 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL, {
-  dropBufferSupport: true,
+const redisUrls = process.env.REDIS_URL!.split(" ");
+
+export const redis = new Redis.Cluster(redisUrls, {
+  redisOptions: {
+    showFriendlyErrorStack: true,
+  },
 });
 
 export function deleteByPattern(r: Redis.Redis, pattern: string) {

bca5fb98c482cdcc7d7fd484566e33c1bf7704ca
Hoang Vo <g@hvvo.dev>
2020-10-04 06:05:16 -0400

    Fix memory leak in PubSub class

--- src/lib/pubsub.ts
+++ src/lib/pubsub.ts
@@ -1,16 +1,22 @@
 import { createClient } from "../db/redis";
 
-export const pubsub = {
-  pub: createClient(),
-  sub: createClient(),
-  _pubsubOnSubChannels: new Set(),
+export class PubSub {
+  pub = createClient();
+  sub = createClient();
+  _channels = new Set<string>();
+  _subscribers = new Set<(inChannel: string, message: string) => void>();
+  constructor() {
+    this.sub.on("message", (channel, message) => {
+      this._subscribers.forEach((pValueFn) => pValueFn(channel, message));
+    });
+  }
   publish(channel: string, message: Record<string, any>) {
     this.pub.publish(channel, JSON.stringify(message));
-  },
+  }
   on<T = any>(
     channel: string,
     filterFn?: (payload: T) => boolean
-  ): AsyncIterableIterator<any> {
+  ): AsyncIterableIterator<T | undefined> {
     const pullQueue: ((value?: any) => void)[] = [];
     const pushQueue: T[] = [];
     let listening = true;
@@ -19,12 +25,12 @@ export const pubsub = {
     // We don't have a lot of channels and likely
     // all are subscribed at one point so we do
     // not need to unsubscribe
-    if (!this._pubsubOnSubChannels.has(channel)) {
+    if (!this._channels.has(channel)) {
       this.sub.subscribe(channel);
-      this._pubsubOnSubChannels.add(channel);
+      this._channels.add(channel);
     }
 
-    function pushValue(inChannel: string, message: string) {
+    const pushValue = (inChannel: string, message: string) => {
       if (inChannel !== channel) return;
       const value: T = JSON.parse(message);
       if (filterFn?.(value) === false) return;
@@ -33,12 +39,16 @@ export const pubsub = {
       } else {
         pushQueue.push(value);
       }
-    }
+    };
 
-    this.sub.on("message", pushValue);
+    this._subscribers.add(pushValue);
+
     const emptyQueue = () => {
       listening = false;
-      this.sub.off("message", pushValue);
+      this._subscribers.delete(pushValue);
+      for (const resolve of pullQueue) {
+        resolve({ value: undefined, done: true });
+      }
       pullQueue.length = 0;
       pushQueue.length = 0;
     };
@@ -65,7 +75,7 @@ export const pubsub = {
         return this;
       },
     };
-  },
-};
+  }
+}
 
-export type PubSub = typeof pubsub;
+export const pubsub = new PubSub();
